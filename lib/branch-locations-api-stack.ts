bdc68c2b2692e67190f8a1e1ca4ed690f4ca900b
Ross White <rosswhite@outlook.com>
2026-09-22 17:48:43 +0100

    feat: adds apiGateway and lambda resources to stack

--- lib/branch-locations-api-stack.ts
+++ lib/branch-locations-api-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class BranchLocationsApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +15,49 @@ export class BranchLocationsApiStack extends cdk.Stack {
       partitionKey: { name: 'LocationId', type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
     });
+
+    const lambdaProps = {
+      handler: 'handler',
+      runtime: lambda.Runtime.NODEJS_22_X,
+      timeout: cdk.Duration.seconds(10),
+      environment: {
+        TABLE_NAME: tableName,
+      },
+    };
+
+    const listLambda = new NodejsFunction(this, 'ListLocationsLambda', {
+      entry: 'lambdas/list.ts',
+      ...lambdaProps,
+    });
+    const getLambda = new NodejsFunction(this, 'GetLocationLambda', {
+      entry: 'lambdas/get.ts',
+      ...lambdaProps,
+    });
+    const createLambda = new NodejsFunction(this, 'CreateLocationLambda', {
+      entry: 'lambdas/create.ts',
+      ...lambdaProps,
+    });
+    const updateLambda = new NodejsFunction(this, 'UpdateLocationLambda', {
+      entry: 'lambdas/update.ts',
+      ...lambdaProps,
+    });
+
+    table.grantReadData(listLambda);
+    table.grantReadData(getLambda);
+    table.grantWriteData(createLambda);
+    table.grantReadWriteData(updateLambda);
+
+    const api = new apigateway.RestApi(this, 'BranchLocationsApi', {
+      restApiName: 'Branch Locations Service',
+      description: 'This service serves branch locations.',
+    });
+
+    const locations = api.root.addResource('locations');
+    locations.addMethod('GET', new apigateway.LambdaIntegration(listLambda));
+    locations.addMethod('POST', new apigateway.LambdaIntegration(createLambda));
+
+    const location = locations.addResource('{locationId}');
+    location.addMethod('GET', new apigateway.LambdaIntegration(getLambda));
+    location.addMethod('PUT', new apigateway.LambdaIntegration(updateLambda));
   }
 }
--- test/branch-locations-api.test.ts
+++ test/branch-locations-api.test.ts
@@ -13,4 +13,68 @@ describe('BranchLocationsApiStack', () => {
 			"BillingMode": "PAY_PER_REQUEST",
 		});
 	});
+
+  it('should create four Lambda functions with the shared runtime, timeout, and TABLE_NAME environment variable', () => {
+    const functions = Object.values(template.findResources('AWS::Lambda::Function'));
+
+    expect(functions).toHaveLength(4);
+    for (const fn of functions) {
+      expect(fn.Properties.Runtime).toBe('nodejs22.x');
+      expect(fn.Properties.Timeout).toBe(10);
+      expect(fn.Properties.Environment.Variables.TABLE_NAME).toBe('BranchLocations');
+    }
+  });
+
+  function policyActionsFor(lambdaConstructId: string): string[] {
+    const policies = template.findResources('AWS::IAM::Policy');
+    const [, policy] = Object.entries(policies).find(([id]) => id.startsWith(lambdaConstructId))!;
+    return policy.Properties.PolicyDocument.Statement[0].Action;
+  }
+
+  it('grants read-only access to the list and get Lambdas', () => {
+    for (const lambdaConstructId of ['ListLocationsLambda', 'GetLocationLambda']) {
+      const actions = policyActionsFor(lambdaConstructId);
+
+      expect(actions).toContain('dynamodb:GetItem');
+      expect(actions).not.toContain('dynamodb:PutItem');
+    }
+  });
+
+  it('grants write-only access to the create Lambda', () => {
+    const actions = policyActionsFor('CreateLocationLambda');
+
+    expect(actions).toContain('dynamodb:PutItem');
+    expect(actions).not.toContain('dynamodb:GetItem');
+  });
+
+  it('grants read and write access to the update Lambda', () => {
+    const actions = policyActionsFor('UpdateLocationLambda');
+
+    expect(actions).toContain('dynamodb:GetItem');
+    expect(actions).toContain('dynamodb:PutItem');
+  });
+
+  it('should create a REST API named "Branch Locations Service"', () => {
+    template.hasResourceProperties('AWS::ApiGateway::RestApi', {
+      Name: 'Branch Locations Service',
+    });
+  });
+
+  it('should create /locations and /locations/{locationId} resources', () => {
+    const resources = Object.values(template.findResources('AWS::ApiGateway::Resource'));
+    const pathParts = resources.map((r: any) => r.Properties.PathPart).sort();
+
+    expect(pathParts).toEqual(['locations', '{locationId}']);
+  });
+
+  it('should create two GET, one POST, and one PUT method, each proxying to a Lambda', () => {
+    template.resourceCountIs('AWS::ApiGateway::Method', 4);
+    template.resourcePropertiesCountIs('AWS::ApiGateway::Method', { HttpMethod: 'GET' }, 2);
+    template.resourcePropertiesCountIs('AWS::ApiGateway::Method', { HttpMethod: 'POST' }, 1);
+    template.resourcePropertiesCountIs('AWS::ApiGateway::Method', { HttpMethod: 'PUT' }, 1);
+
+    template.resourcePropertiesCountIs('AWS::ApiGateway::Method', {
+      Integration: { Type: 'AWS_PROXY', IntegrationHttpMethod: 'POST' },
+    }, 4);
+  });
 });
